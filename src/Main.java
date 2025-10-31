3ca493994680579c1a5c78e0859eef4f1db55ce1
joonlee <96dalee@gmail.com>
2025-10-31 09:47:39 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import facade.HomeTheaterFacade;
+
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("스마트 홈 시어터 시스템 데모 시작\n");
+        }
+
+        private static HomeTheaterFacade homeTheaterFacade;
+        private static Thea
+    }
+}
